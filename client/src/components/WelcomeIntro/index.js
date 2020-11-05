998b745d76980e4e42f7dd2255418ad7d0e01149
Eric Folenta <ericfolenta@gmail.com>
2020-11-04 19:54:47 -0800

    added intro text

---
+++ client/src/components/WelcomeIntro/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./style.css";
+
+import video from "../../assets/production_ID_4281239.mp4"
+
+// The ...props means, spread all of the passed props onto this element
+// That way we don't have to define them all individually
+function WelcomeIntro(props) {
+    return (
+        <div className="container">
+            <div className="row">
+                <p class="text-center">Welcome to "Inventory Tracker", an easy to use inventory management system that allows you to excel at warehouse efficiency. Wether you run logistics in a warehouse or keeping track of coffee beans, our simple to use program allows anyone to easily manage and track their inventory. We offer a solution for anyone!</p>
+            </div>
+        </div>
+    );
+}
+
+export default WelcomeIntro;
