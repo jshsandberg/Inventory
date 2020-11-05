0cec19b4830a0e0823ace32dc34144d8274c81a3
Eric Folenta <ericfolenta@gmail.com>
2020-11-04 19:55:26 -0800

    added style changes

--- client/src/components/WelcomeIntro/index.js
+++ client/src/components/WelcomeIntro/index.js
@@ -9,7 +9,7 @@ function WelcomeIntro(props) {
     return (
         <div className="container">
             <div className="row">
-                <p class="text-center">Welcome to "Inventory Tracker", an easy to use inventory management system that allows you to excel at warehouse efficiency. Wether you run logistics in a warehouse or keeping track of coffee beans, our simple to use program allows anyone to easily manage and track their inventory. We offer a solution for anyone!</p>
+                <p className="text-center">Welcome to "Inventory Tracker", an easy to use inventory management system that allows you to excel at warehouse efficiency. Wether you run logistics in a warehouse or keeping track of coffee beans, our simple to use program allows anyone to easily manage and track their inventory. We offer a solution for anyone!</p>
             </div>
         </div>
     );
