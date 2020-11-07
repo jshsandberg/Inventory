85cd0d9b417bea6b2378d75caa7e3792da094df3
Eric Folenta <ericfolenta@gmail.com>
2020-11-07 10:24:15 -0800

    added base layout

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./style.css";
+
+function Footer () {
+
+
+  return (
+    <>
+        <div className="row">
+            <div className="col-4">
+                Links to Pages
+            </div>
+            <div className="col-4">
+                Logo
+            </div>
+            <div className="col-4">
+                Project Members
+            </div>
+        </div>
+    </>
+  )
+}
+
+export default Footer;
