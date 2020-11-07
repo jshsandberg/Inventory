2bc0d756f475cc90f0d13aabe338a4059118d7fc
Crys <hancc1006@gmail.com>
2020-11-07 11:48:09 -0800

    added authentication

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,18 +10,18 @@ module.exports = {
       .catch(err => res.status(422).json(err));
   },
   create: function(req, res) {
-    let newUser = new User({
-      username: req.body.username,
-      password: req.body.password
-    });
-    newUser.save((err,user)=>{
-      if(err){throw err;}
-      res.json(user);
-    });
-    // db.User
-    //   .create(req.body)
-    //   .then(dbModel => res.json(dbModel))
-    //   .catch(err => res.status(422).json(err));
+    // let newUser = new User({
+    //   username: req.body.username,
+    //   password: req.body.password
+    // });
+    // newUser.save((err,user)=>{
+    //   if(err){throw err;}
+    //   res.json(user);
+    // });
+    db.User
+      .create(req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err));
   },
   update: function(req, res) {
     db.User
