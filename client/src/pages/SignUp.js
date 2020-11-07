68f37c2e473cfb71a03dfc342a18f6b848d0590e
Eric Folenta <ericfolenta@gmail.com>
2020-11-07 11:08:39 -0800

    added components to pages

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,9 +1,14 @@
 import React from "react";
+import Footer from "../components/Footer";
 import SignUp from "../components/SignUp/index";
+import WelcomeNavBar from "../components/WelcomeNavBar";
 
 function SignInPage() {
   return(
+    <>
+    <WelcomeNavBar />
     <SignUp />
+    <Footer />
   );
 }
 
