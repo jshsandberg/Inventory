0e0cef8c73fdd997ceacba6a8e18a1d0dc388813
jshsandberg <jshsandberg@gmail.com>
2020-11-09 18:33:56 -0800

    working

---
+++ client/src/utils/mockData.js
@@ -0,0 +1,128 @@
+export const inventory = 
+{
+    businessName: "Coffee Shop",
+    inventory: [
+        {
+            item: "beans",
+            dateAdded: "09/20/2020",
+            quantity: 2,
+            cycle: [
+                {
+                    amount: 5
+                },
+                {
+                    every: 30
+                }
+            ],
+            sale:
+                {
+                    unitPrice: 2.99,
+                    salePrice: 4.59,
+                    unitsSold: 10
+                }
+            
+        },
+        {
+            item: "milk",
+            dateAdded: "10/15/2020",
+            quantity: 5,
+            cycle: [
+                {
+                    amount: 5
+                },
+                {
+                    every: 30
+                }
+            ],
+            sale:
+            {
+                unitPrice: 2.99,
+                salePrice: 4.59,
+                unitsSold: 10
+            }
+        
+        },   
+        {
+            item: "eggs",
+            dateAdded: "10/05/2020",
+            quantity: 12,
+            cycle: [
+                {
+                    amount: 5
+                },
+                {
+                    every: 30
+                }
+            ],
+            sale:
+            {
+                unitPrice: 2.99,
+                salePrice: 4.59,
+                unitsSold: 10
+            }
+        
+        },  
+        {
+            item: "cups",
+            dateAdded: "08/20/2020",
+            quantity: 25,
+            cycle: [
+                {
+                    amount: 5
+                },
+                {
+                    every: 30
+                }
+            ],
+            sale:
+            {
+                unitPrice: 2.99,
+                salePrice: 4.59,
+                unitsSold: 10
+            }
+        
+        },   
+        {
+            item: "forks",
+            dateAdded: "10/20/2020",
+            quantity: 85,
+            cycle: [
+                {
+                    amount: 5
+                },
+                {
+                    every: 30
+                }
+            ],
+            sale:
+            {
+                unitPrice: 2.99,
+                salePrice: 4.59,
+                unitsSold: 10
+            }
+        
+        }, 
+        {
+            item: "spoons",
+            dateAdded: "09/09/2020",
+            quantity: 9,
+            cycle: [
+                {
+                    amount: 5
+                },
+                {
+                    every: 30
+                }
+            ],
+            sale:
+            {
+                unitPrice: 2.99,
+                salePrice: 4.59,
+                unitsSold: 10
+            }
+        
+        },
+    ]
+};
+
+
