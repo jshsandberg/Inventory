471371310038f83421801ef8d198c814ea25682c
Eric Folenta <ericfolenta@gmail.com>
2020-11-11 20:28:26 -0800

    added Welcome Promo component and created the shell

---
+++ client/src/components/WelcomePromo/index.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+
+function WelcomePromo(props) {
+    return (
+        <div className="container-fluid" id="promo-container">
+            <div className="row" id="promo">
+                <div className="col-md-8 col-sm-12">
+
+                </div>
+                <div className="col-md-4 col-sm-12">
+
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default WelcomePromo;
