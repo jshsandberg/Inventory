a7b9f0ebd2df646c7cc7c18c517e4d8cc86d1a37
Eric Folenta <ericfolenta@gmail.com>
2020-11-11 20:48:44 -0800

    replaces class with classname

--- client/src/components/WelcomeFeatures/index.js
+++ client/src/components/WelcomeFeatures/index.js
@@ -14,27 +14,27 @@ function WelcomeFeatures(props) {
             </div>
             <div className="row features">
                 <div className="col-md-4">
-                <div class="card">
-                <div class="card-body">
-                    <i class="fa fa-fighter-jet" aria-hidden="true"></i>
+                <div className="card">
+                <div className="card-body">
+                    <i className="fa fa-fighter-jet" aria-hidden="true"></i>
                     <h2>Efficient</h2>
                     <p>Woosh! The sounds of speedy and lightweight. It goes together like peanut butter and jelly. It's just a win win!</p>
                 </div>
                 </div>
                 </div>
                 <div className="col-md-4" id="">
-                <div class="card">
-                <div class="card-body">
-                    <i class="fa fa-shield" aria-hidden="true"></i>
+                <div className="card">
+                <div className="card-body">
+                    <i className="fa fa-shield" aria-hidden="true"></i>
                     <h2>Secure</h2>
                     <p>Using user authentication protocols to keep your data secure. Only you have access to your data.</p>
                 </div>
                 </div>
                 </div>
                 <div className="col-md-4">
-                <div class="card">
-                <div class="card-body">
-                    <i class="fa fa-thumbs-up" aria-hidden="true"></i>
+                <div className="card">
+                <div className="card-body">
+                    <i className="fa fa-thumbs-up" aria-hidden="true"></i>
                     <h2>Easy UI</h2>
                     <p>Ads? Gross! Our easy to use interface is not cluttered. Find your way to your items quickly and easily.</p>
                 </div>
