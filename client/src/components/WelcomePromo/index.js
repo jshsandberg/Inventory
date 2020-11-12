20c772ebf2e2735da8d81f0fd26198725fc8fdc1
Eric Folenta <ericfolenta@gmail.com>
2020-11-11 20:29:41 -0800

    added text and button

--- client/src/components/WelcomePromo/index.js
+++ client/src/components/WelcomePromo/index.js
@@ -6,10 +6,10 @@ function WelcomePromo(props) {
         <div className="container-fluid" id="promo-container">
             <div className="row" id="promo">
                 <div className="col-md-8 col-sm-12">
-
+                    <h2>Excited to get started? We thought so!</h2>
                 </div>
                 <div className="col-md-4 col-sm-12">
-
+                    <button>Sign Up</button>
                 </div>
             </div>
         </div>
