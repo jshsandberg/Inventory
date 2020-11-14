3b799739c7b754881cf5032842be0cc767c4a633
b1acksmyth <smith.spence13@gmail.com>
2020-11-14 10:34:42 -0800

    format and function rename

--- client/src/components/SignIn/index.js
+++ client/src/components/SignIn/index.js
@@ -1,32 +1,30 @@
 import React from "react";
 import "./style.css";
 
-function SignUp () {
+function SignIn () {
 
 
   return (
     <div className="container">
-    <div className="row">
-    <div className="col-12">
-      <form className="form-signin">
-        <h1 className="h3 mb-3">Please sign in...</h1>
-        <label for="inputEmail" className="sr-only">Email address</label>
-        <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
-        <label for="inputPassword" className="sr-only">Password</label>
-        <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
-        <div className="checkbox mb-3">
-          <label>
-            <input type="checkbox" value="remember-me" /> Remember me
-          </label>
+      <div className="row">
+        <div className="col-12">
+          <form className="form-signin">
+            <h1 className="h3 mb-3">Please sign in...</h1>
+            <label for="inputEmail" className="sr-only">Email address</label>
+            <input type="email" id="inputEmail" className="form-control" placeholder="Email address" required autofocus />
+            <label for="inputPassword" className="sr-only">Password</label>
+            <input type="password" id="inputPassword" className="form-control" placeholder="Password" required />
+            <div className="checkbox mb-3">
+              <label>
+                <input type="checkbox" value="remember-me" />Remember me
+              </label>
+            </div>
+            <button className="inverted" id="signup-login-btn" type="submit">Sign In <i class="fa fa-sign-in" aria-hidden="true"></i></button>
+          </form>
         </div>
-        <button className="inverted" id="signup-login-btn" type="submit">Sign In <i class="fa fa-sign-in" aria-hidden="true"></i>
-</button>
-        <p className="mt-5 mb-3 text-muted">&copy; 2017-2020</p>
-      </form>
       </div>
-      </div>
-      </div>
+    </div>
   )
 }
 
-export default SignUp;
+export default SignIn;
