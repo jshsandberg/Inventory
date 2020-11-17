aba9c27f8da59cbf0c71deef52bf8b433ba81796
Eric Folenta <ericfolenta@gmail.com>
2020-11-16 21:24:00 -0800

    updated fill page with components

--- client/src/pages/InventoryFill.js
+++ client/src/pages/InventoryFill.js
@@ -1,14 +1,25 @@
 import React from "react";
 import NavBar from "../components/NavBar/index"
 import InventoryForm from "../components/InventoryForm/index"
+import SideBar from "../components/SideBar";
 
 function InventoryFill() {
 
     return (
-        <div>
-            <NavBar></NavBar>
-            <InventoryForm></InventoryForm>
-        </div>
+        <>
+        <NavBar />
+        <div className="container-fluid" id="inventory-page">
+          <div className="row">
+            <div className="col-2">
+                <SideBar />
+            </div>
+            <div className="col-10">
+              <InventoryForm></InventoryForm>
+            </div>
+          </div>
+        </div>  
+        </>
+
     )
 };
 
